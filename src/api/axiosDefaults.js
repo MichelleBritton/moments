85e7faa079f892220709bce89fd8306c8ee64293
Michelle Britton <michelle@daisy-webdesign.co.uk>
2024-02-16 11:53:10 +0000

    commit

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+// Set the baseURL of the deployed api 
+// Sent content-type to multipart/form-data as that is the data format the api will be expecting. 
+// We need multipart because our app will be dealing with images as well as text in its requests
+// To avoid any CORS errors when sending cookies, we also need to set withCredentials to true
+axios.defaults.baseURL = 'https://djangorf-api-76c3c6fb6902.herokuapp.com/'
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.withCredentials = true
